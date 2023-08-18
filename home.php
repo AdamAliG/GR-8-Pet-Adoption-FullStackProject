2949e5b9354d24da8bbdb7fe5e99f2c22bf18e6b
FabiWetz23 <139136925+FabiWetz23@users.noreply.github.com>
2023-08-18 11:38:15 +0200

    Home and Details Updated

--- home.php
+++ home.php
@@ -1,76 +1,200 @@
 <?php
-    session_start(); 
+session_start(); 
 
-    require_once "db_connect.php";
+if(!isset($_SESSION["user"]) && !isset($_SESSION["admin"])){ 
+    header("Location: user_auth/login.php"); 
+}
 
+
+require_once "db_connect.php";
+
+$sql = "SELECT * FROM pets WHERE 1"; 
+
+if (isset($_GET['species'])) {
+    $selectedSpecies = $_GET['species'];
+    if ($selectedSpecies !== 'all') {
+        $sql .= " AND species = '$selectedSpecies'";
+    }
+}
+if (isset($_GET['size'])) {
+    $selectedSize = $_GET['size'];
+    if ($selectedSize !== 'all') {
+        $sql .= " AND size = '$selectedSize'";
+    }
+}
+if (isset($_GET['status'])) {
+    $selectedStatus = $_GET['status'];
+    if ($selectedStatus !== 'all') {
+        $sql .= " AND status = '$selectedStatus'";
+    }
+}
+
+$result = mysqli_query($connection, $sql);
+
+$cards = "";
+
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            $cards .= "<div class='col mb-4'>
+                <div class='card h-100'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>{$row["name"]}</h5>
+                        
+                    <img src='pictures/{$row["image"]}' class='card-img-top' alt='...'>
+                    
+                        <p class='card-text'>Species: {$row["species"]}</p>
+                        <p class='card-text'>Location: {$row["location"]}</p>
+                        <a href='details.php?id={$row["id"]}' class='btn btn-info'>Show Details</a>
+                    </div>
+                </div>
+            </div>";
+        }
+    } else {
+        $cards = "<p>No results found</p>";
+    }
     $sql = "SELECT * FROM users WHERE id = {$_SESSION["user"]}";
     $result = mysqli_query($connection, $sql);
     $row = mysqli_fetch_assoc($result);
+    mysqli_close($connection);
 
-$query = "SELECT * FROM pets";
-$result = $connection->query($query);
-
-    //mysqli_close($connection);
-
-    
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome <?= $row["username"] ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pet Adoption</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+    
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="pictures/<?= $row["pictures"] ?>" alt="user pic" width="30" height="24">
-            </a>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
 
-                <li class="nav-item">
-                <a class="nav-link" href="login_logout_register_crud/update.php?id=<?= $row["id"] ?>">edit</a>
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Pet Adoption</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
                 </li>
-
                 <li class="nav-item">
-                    <a class="nav-link" href="user_crud/logout.php?logout">Logout</a>
+                    <a class="nav-link" href="#">Adoption Stories</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pet of the Day</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Calendar for Volunteers</a>
                 </li>
             </ul>
+            
+            <ul class="navbar-nav ms-auto">
+            <a class="nav-item me-3" href="#">
+                <img src="pictures/<?= $row["pictures"] ?>" alt="user pic" width="35" height="30">
+            </a>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="user_auth/update.php?id=<?= $row["id"] ?>">Update</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="user_auth/logout.php?logout">Logout</a>
+                    </li>
+                </ul>
+            
         </div>
-    </nav>
-    <table>
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Species</th>
-            <th>Breed</th>
-            <th>Age</th>
-            <th>Location</th>
-            <th>Description</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php while($petRow = $result->fetch_assoc()): ?>
-            <tr>
-                <td><?= $petRow['name'] ?></td>
-                <td><?= $petRow['species'] ?></td>
-                <td><?= $petRow['description'] ?></td>
-                <td><?= $petRow['location'] ?></td>
-                
-                
-            </tr>
-        <?php endwhile; ?>
-    </tbody>
-</table>
-    <h2 class="text-center">Welcome <?= $row["username"] ?></h2>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </div>
+</nav>
+
+
+<div class="container mt-5">
+    <h1 class="mt-5">Pet List</h1>
+    <form action="" method="get" class="mb-3">
+        <div class="accordion accordion-flush" id="filterAccordion">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="filterHeader">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#filterCollapse" aria-expanded="false" aria-controls="filterCollapse">
+                        Filter Options
+                    </button>
+                </h2>
+                <div id="filterCollapse" class="accordion-collapse collapse" aria-labelledby="filterHeader" data-bs-parent="#filterAccordion">
+                    <div class="accordion-body">
+                        <div class="mb-3">
+                            <label for="speciesSelect" class="form-label">Species:</label>
+                            <select name="species" id="speciesSelect" class="form-select">
+                                <option value="all">All</option>
+                                <option value="dog">Dog</option>
+                                <option value="cat">Cat</option>
+                                <option value="bird">Bird</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="sizeSelect" class="form-label">Size:</label>
+                            <select name="size" id="sizeSelect" class="form-select">
+                                <option value="all">All</option>
+                                <option value="small">Small</option>
+                                <option value="medium">Medium</option>
+                                <option value="big">Big</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="statusSelect" class="form-label">Status:</label>
+                            <select name="status" id="statusSelect" class="form-select">
+                                <option value="all">All</option>
+                                <option value="not adopted">Not adopted</option>
+                                <option value="adopted">Adopted</option>
+                                <option value="pending">Pending</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-sm mt-3">Apply Filters</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    
+    <div class="input-group input-group-sm mb-3">
+        <input type="text" id="searchInput" class="form-control" placeholder="Search by name...">
+    </div>
+    
+    <div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 row-cols-xs-1">
+        <?= $cards ?>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    const searchInput = document.getElementById('searchInput');
+    const cardContainer = document.querySelector('.row-cols');
+    const cards = Array.from(document.querySelectorAll('.col.mb-4'));
+
+    searchInput.addEventListener('input', function () {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        cards.forEach(card => {
+            const petName = card.querySelector('.card-title').textContent.toLowerCase();
+            const shouldShow = petName.includes(searchTerm);
+
+            card.style.display = shouldShow ? 'block' : 'none';
+        });
+        cards.sort((a, b) => {
+            const isVisibleA = a.style.display === 'block';
+            const isVisibleB = b.style.display === 'block';
+            return isVisibleB - isVisibleA;
+        });
+
+        cards.forEach(card => cardContainer.removeChild(card));
+        cards.forEach(card => cardContainer.appendChild(card));
+    });
+</script>
+
+
 </body>
 </html>
