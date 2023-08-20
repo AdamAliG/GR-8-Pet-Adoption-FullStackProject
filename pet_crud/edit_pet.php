f13aa052611a78101aee25c28aa56c7a8d694f1a
AdamAliG <electron.adam@proton.me>
2023-08-21 00:21:25 +0200

    resolved smallerrors increased function in petCRUD

--- pet_crud/edit_pet.php
+++ pet_crud/edit_pet.php
@@ -2,6 +2,9 @@
 require_once "../db_connect.php";
 require_once "../file_upload.php";
 
+$message = "";
+
+
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
@@ -12,14 +15,20 @@ if (isset($_GET['id'])) {
         $location = $_POST['location'];
         $breed = $_POST['breed'];
         $age = $_POST['age'];
-        $image = fileUpload($_FILES["pictures"],'pet');
-
         $size =  $_POST['size'];
         $status = $_POST['status'];
 
-        $query = "UPDATE pets SET name='$name', species='$species', description='$description', location='$location', breed='$breed', age ='$age', image='$image[0]', size= '$size', status = '$status'  WHERE id='$id'";
+        if ($_FILES["pictures"]["error"] != UPLOAD_ERR_NO_FILE) {
+            $image = fileUpload($_FILES["pictures"], 'pet');
+            $query = "UPDATE pets SET name='$name', species='$species', description='$description', location='$location', breed='$breed', age='$age', image='$image[0]', size='$size', status='$status' WHERE id='$id'";
+        } else {
+            $query = "UPDATE pets SET name='$name', species='$species', description='$description', location='$location', breed='$breed', age='$age', size='$size', status='$status' WHERE id='$id'";
+        }
+
+        
         if ($connection->query($query)) {
-            echo "Pet updated successfully!";
+            $message = "Pet updated successfully!";
+
             header("Refresh: 2; url= ../dashboard.php");
         } else {
             echo "Error updating pet!";
@@ -44,47 +53,91 @@ if (isset($_GET['id'])) {
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../styles.css">
+    <style>
+    body, html {
+        height: 100%;
+        margin: 0;
+    }
+    .custom-input-width {
+    width: 100% !important;
+}
+    .centered-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    }
+
+    #success-message {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0.5;
+        transition: opacity 2s;
+    }
+</style>
+
 </head>
 <body>
 
 <h1 class="text-center">Edit Pet!</h1>
 
-<div class="container">
+<div class="centered-container">
+<div id="success-message" style="display: <?= ($message == "Pet updated successfully!") ? 'flex' : 'none'; ?>;">
+    <h1 class="text-center"><?= $message; ?></h1>
+</div>
+<div style="display: <?= ($message == "Pet updated successfully!") ? 'none' : 'block'; ?>;">
   <form method="post" enctype="multipart/form-data">
 
   <label for="name" class="form-label">Name:</label>
-    <input type="text" name="name" class="form-control" value="<?= $pet['name'] ?>">
+    <input type="text" name="name" class="form-control custom-input-width" value="<?= $pet['name'] ?>">
 
     <label for="species" class="form-label">species:</label>
-    <input type="text" name="species" class="form-control" value="<?= $pet['species'] ?>">
+    <input type="text" name="species" class="form-control custom-input-width" value="<?= $pet['species'] ?>">
 
     <label for="description" class="form-label">Description: </label>
-    <textarea name="description" class="form-control" ><?= $pet['description'] ?></textarea>
+    <textarea name="description" class="form-control custom-input-width" ><?= $pet['description'] ?></textarea>
 
     <label for="Location" class="form-label">Location: </label>
-    <input type="text" name="location" class="form-control" value="<?= $pet['location'] ?>">
+    <input type="text" name="location" class="form-control custom-input-width" value="<?= $pet['location'] ?>">
 
     <label for="breed" class="form-label">Breed:</label>
-<input type="text" name="breed" class="form-control" value="<?= $pet['breed'] ?>">
+<input type="text" name="breed" class="form-control custom-input-width" value="<?= $pet['breed'] ?>">
 
 
     <label for="status" class="form-label">status:</label>
-    <input type="text" name="status" class="form-control" value="<?= $pet['status'] ?>">
+    <input type="text" name="status" class="form-control custom-input-width" value="<?= $pet['status'] ?>">
 
     <label for="age" class="form-label">age:</label>
-    <input type="text" name="age" class="form-control" value="<?= $pet['age'] ?>">
+    <input type="text" name="age" class="form-control custom-input-width" value="<?= $pet['age'] ?>">
 
     <label for="pictures" class="form-label">image:</label>
-    <input type="file" class="form-control w-25" id="pictures" name="pictures">
+    <input type="file" class="form-control custom-input-width" id="pictures" name="pictures">
 
 
     <label for="size" class="form-label">size:</label>
-    <input type="text" name="size" class="form-control" value="<?= $pet['size'] ?>">
+    <input type="text" name="size" class="form-control custom-input-width" value="<?= $pet['size'] ?>">
 
     <input type="submit" class="btn btn-primary" name="update" value="Update">
 </form>  
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    window.onload = function() {
+        let successMessage = document.getElementById('success-message');
+        if (successMessage) {
+            setTimeout(function() {
+                successMessage.style.opacity = "1";
+            }, 100);
+        }
+    };
+</script>
 </body>
 </html>
 
