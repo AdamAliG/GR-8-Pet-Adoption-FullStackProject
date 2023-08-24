a4431545fc78516364e01b34a1d44139e985a794
Mehdi Salimi <salimimehdi@yahoo.com>
2023-08-24 12:04:30 +0200

    last

--- user_crud/details_user.php
+++ user_crud/details_user.php
@@ -129,54 +129,9 @@ if (isset($_GET["id"])) {
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container">
-        <a class="navbar-brand" href="#">Pet Adoption</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="../dashboard.php?dashboard">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="adoption_stories/adoption_stories.php">Adoption Stories</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="pet_of_the_day/pet_of_the_day.php">Pet of the Day</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="calendar/calendar.php">Calendar for Volunteers</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="matchmaker/matchmaker.html">Matchmaker</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../pet_crud/add_pet.php?add_pet">Add Pet</a>
-                </li>
-
-            </ul>
-            
-            <ul class="navbar-nav ms-auto">
-            <a class="nav-item me-3" href="#">
-                <img src="../public/images/user_images/<?= $adminRow["pictures"] ?>" alt="user pic" width="35" height="30">
-            </a>
-                    <li class="nav-item">
-                        <a class="nav-link font-weight-bold" href="../user_auth/update.php?id=<?= $adminRow["id"] ?>">Update</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link font-weight-bold" href="users.php?users">See Users</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link font-weight-bold" href="../user_auth/logout.php?logout">Logout</a>
-                    </li>
-                </ul>
-            
-        </div>
-    </div>
-</nav>
+<?php
+require_once "../navbar_admin_sub.php";
+?> 
 <div class="container w-50 mt-5">
     <h2>User Details</h2>
     <div class="card shadow"> 
